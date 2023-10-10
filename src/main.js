399f7f832348eb8050c2ef886e51c859f5df3158
ファイザル アバディ <abadi.faizal@i-studio.co.jp>
2023-10-10 12:17:01 +0900

    finishing vue mastery lesson 8 about flash message

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from "vue";
+import { createApp, reactive } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
 app.use(router);
+
+// Create a reactive object
+const GStore = reactive({ flashMessage: "" });
+app.provide("GStore", GStore); // provide this object so others can inject it
 
 app.mount("#app");
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,13 +1,23 @@
 <script setup>
 import { useRouter } from "vue-router";
 import { defineProps } from "vue";
+import { inject } from "vue";
 
-defineProps(["event"]);
+const props = defineProps(["event"]);
+
 const router = useRouter();
+
+const GStore = inject("GStore");
 
 const register = () => {
   // Call to API
   // If registered then redirect to event details
+  console.log(props.event.title);
+  GStore.flashMessage =
+    "You are successfully registered for " + props.event.title;
+  setTimeout(() => {
+    GStore.flashMessage = "";
+  }, 3000);
   router.push({
     name: "EventDetails",
   });
